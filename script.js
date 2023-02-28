23efbd420b52268238d49d3324a94656ae98c87a
Julie Pitney <julie@pitney.org>
2023-02-28 15:12:12 -0600

    First Commit of entire project

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// TODO: add code here
+window.addEventListener("load", function(event){
+        fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+            // console.log(response);
+            response.json().then(function(json){
+                console.log(json);
+                
+              let astronautContainer = document.getElementById("container");
+              for(let i=0; i< json.length; i++){
+                let newString= `
+                <div class = "astronaut">
+                 <div class = "bio">
+                      <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                     <ul>
+                       <li>Hours in space: ${json[i].hoursInSpace}</li>
+                       <li>Active: ${json[i].active}</li>
+                       <li>Skills: ${json[i].skills}</li>
+                     </ul>
+                     </div>
+                     <img class ="avatar" src =${json[i].picture}>
+                    </div>`
+            
+                    astronautContainer.innerHTML += newString;
+                 
+              }
+            })
+          })
+        }) 
